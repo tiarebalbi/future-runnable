4a4d1ad45dc780e00a60c83f032b36ed8076d773
Tiarê Balbi Bonamini <tiarebalbi@me.com>
2013-10-23 21:13:18 -0200

    Configuração do Projeto

---
+++ src/main/java/com/tiarebalbi/Execution.java
@@ -0,0 +1,5 @@
+package com.tiarebalbi;
+
+public class Execution {
+
+}
---
+++ src/main/java/com/tiarebalbi/sample/Logic2.java
@@ -0,0 +1,11 @@
+package com.tiarebalbi.sample;
+
+public class Logic2 {
+	
+	public void executar(int tamanho) {
+		for(int i = 0; i< tamanho;i++) {
+			System.out.println("Rodou : " + i);
+		}
+	}
+
+}
